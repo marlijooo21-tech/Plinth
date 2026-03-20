3ef80f42d23cb46879d71ee40cad8415c7671904
marlijooo21-tech <marlijooo21@gmail.com>
2026-03-20 10:07:43 -0700

    Update generate.js

--- plinth/api/generate.js
+++ plinth/api/generate.js
@@ -1,4 +1,6 @@
-export default async function handler(req, res) {
+export const config = { maxDuration: 60 }
+        
+     export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' })
   }
